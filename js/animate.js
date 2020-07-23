38cd04acb8f8bde059c19efc5e6b9917d0a490ea
nikhilupadhyay24 <nikhilupadhyay24@gmail.com>
2020-07-23 17:18:24 +0530

    23jul

---
+++ js/animate.js
@@ -0,0 +1,29 @@
+
+var i = 0;
+var j=0;
+var k=0;
+var txt1 = 'Hey,';
+var txt2='I am';
+var txt3='Nikhil.';
+var speed = 300;
+
+window.onload=function typeWriter() {
+  if (i < txt1.length) {
+    document.getElementById("line1").innerHTML += txt1.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+  else if (j< txt2.length) {
+    document.getElementById("line2").innerHTML += txt2.charAt(j);
+    j++;
+    setTimeout(typeWriter, speed);
+  }
+  else if(k < txt3.length) {
+    document.getElementById("line3").innerHTML += txt3.charAt(k);
+    k++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+
+
